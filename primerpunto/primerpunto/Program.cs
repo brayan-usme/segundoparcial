d27724b30a1bc5a8d6505b077f70b3ed78dd1fb6
brayan usme <brayanelgamer2@gmail.com>
2024-10-07 22:09:44 -0500

    oo

--- primerpunto/primerpunto/Program.cs
+++ primerpunto/primerpunto/Program.cs
@@ -13,5 +13,27 @@ class AdivinaElNumero
             int numeroJugadores = ObtenerNumeroDeJugadores();
             int rangoMaximo = DeterminarRangoMaximo(numeroJugadores);
             int numeroAleatorio = GenerarNumeroAleatorio(rangoMaximo);
-        }
+            while (!ganadorEncontrado)
+            {
+                for (int i = 1; i <= numeroJugadores; i++)
+                {
+                    turno++;
+                    Console.WriteLine($"\nTurno {turno} - Jugador {i}, es tu turno.");
+                    Console.Write("Adivina el número: ");
+                    int intento = Convert.ToInt32(Console.ReadLine());
+
+                    if (intento == numeroAleatorio)
+                    {
+                        Console.WriteLine("¡HAS GANADO, JUGADOR {0}!", i);
+                        ganadorEncontrado = true;
+                        break;
+                    }
+                    else if (intento > numeroAleatorio)
+                    {
+                        Console.WriteLine("MENOR");
+                    }
+                    else
+                    {
+                        Console.WriteLine("MAYOR");
+                    }
 }
